e9282654e8d89db7540f6b5090c74aa725d2d298
MonsieurVictor <34025717+MonsieurVictor@users.noreply.github.com>
2018-12-08 02:29:39 +0300

    added forms and boiling-calc
    added forms and boiling-calc

--- react_redux_weather_app-master/README.md
+++
@@ -1,3 +0,0 @@
-## You can look at the result here: http://rs-weather.surge.sh/
-Also feel free to visit http://vradov.space/
-That is where I hide my pancakes and punch goblins!
---
+++ weather-lesson/src/App.js
@@ -0,0 +1,95 @@
+import React from "react";
+import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
+
+const API_KEY = "2f776effac2ead4f0e22df37f62db36b";
+
+class App extends React.Component {
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+    error: undefined
+  }
+
+  gettingweather = async (e) => {
+
+    e.preventDefault();
+    var city=e.target.elements.city.value;
+    if(city) {
+      try {
+        const api_url = await
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+      
+        const data = await api_url.json();
+        console.log(data);
+
+        var date = new Date();
+        var sunrise = data.sys.sunrise*1000;
+        date.setTime(sunrise);
+        var sunrise_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+        var sunset = data.sys.sunset*1000;
+        date.setTime(sunset);
+        var sunset_date= date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+        this.setState ({
+          temp: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          pressure: data.main.pressure,
+          sunrise: sunrise_date,
+          sunset: sunset_date,
+          error: undefined
+        });
+      } 
+      catch (e) {
+        alert("Город не найден!")
+      }
+    } else {
+      this.setState ({
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        pressure: undefined,
+        sunrise: undefined,
+        sunset: undefined,
+        error: "Введите название города"
+      })
+  }
+};
+
+
+  render () {
+    return(
+      <div className="wrapper">
+        <div className="main">
+            <div className="contaier">
+              <div className="row"> 
+                <div className="col-sm-5 info">
+                  <Info />
+                </div>
+                <div classname="col-sm-7">
+                  <Form weatherMethod={this.gettingweather}/>
+                  <Weather
+                    temp={this.state.temp}
+                    city={this.state.city}
+                    country={this.state.country}
+                    pressure={this.state.pressure}
+                    sunrise={this.state.sunrise}
+                    sunset={this.state.sunset}
+                    error={this.state.error}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>    
+    )
+  }
+}
+
+export default App;
